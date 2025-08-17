4f67f975c046e8ea6b68a2eebad73a7206430c86
Sarasmishra <Sarasmishra8100@gmail.com>
2025-08-17 12:34:34 +0530

    file updated

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const userModel = require('../models/user.model')
 const { SALTROUND } = require('../config/config')
 const generate = require('../utils/jwtToken')
